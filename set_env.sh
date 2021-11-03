188621ec88c107bf3fcc59a327bca349d6735051
nour elbana <n.ali@egyptsmartcards.com>
2021-11-03 14:25:58 +0200

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=database-1.cyz5794zrnak.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=uda-buck
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
